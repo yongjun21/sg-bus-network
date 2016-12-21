738260cb93477ecf09726e041f97ef4d469324e5
Yong Jun <yjthong.2009@smu.edu.sg>
2016-12-21 16:23:02 +0800

    Long routes directed graph

--- src/buildGraph.js
+++ src/buildGraph.js
@@ -4,11 +4,14 @@ import BusRoutes from '../data/BusRoutes.json'
 import BusStops from '../data/BusStops.json'
 const keyedBusStops = _.keyBy(BusStops, 'BusStopCode')
 
-const graph = buildGraph()
-fillLatLng(graph)
-fs.writeFileSync('data/graph.json', JSON.stringify(graph, null, '\t'))
+// const graph = buildGraph()
+// fillLatLng(graph)
+// fs.writeFileSync('data/graph.json', JSON.stringify(graph, null, '\t'))
 
-export default function buildGraph () {
+const graph2 = buildGraph2()
+fs.writeFileSync('data/graph2.json', JSON.stringify(graph2, null, '\t'))
+
+export function buildGraph () {
   const graph = {}
   const grouped = _.groupBy(BusRoutes,
     route => route.ServiceNo + '.' + route.Direction)
@@ -20,6 +23,25 @@ export default function buildGraph () {
       const edge = start + '.' + end
       if (!(edge in graph)) graph[edge] = {start, end, service: []}
       graph[edge].service.push(key)
+    }
+  })
+  return _.values(graph)
+}
+
+export function buildGraph2 () {
+  const graph = {}
+  const grouped = _.groupBy(BusRoutes,
+    route => route.ServiceNo + '.' + route.Direction)
+  Object.keys(grouped).forEach(key => {
+    const sorted = _.sortBy(grouped[key], 'StopSequence')
+    for (let i = 0; i < sorted.length - 1; i++) {
+      for (let j = i + 1; j < sorted.length; j++) {
+        const start = sorted[i].BusStopCode
+        const end = sorted[j].BusStopCode
+        const edge = start + '.' + end
+        if (!(edge in graph)) graph[edge] = {start, end, service: []}
+        graph[edge].service.push(key)
+      }
     }
   })
   return _.values(graph)
